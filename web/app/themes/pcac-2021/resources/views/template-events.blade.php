866280975887d63a112efe6097668ffe11ebff4a
Francois Huyghe <francois@13milliseconds.com>
2021-06-17 14:43:37 -0400

    Events Calendar template

---
+++ web/app/themes/pcac-2021/resources/views/template-events.blade.php
@@ -0,0 +1,13 @@
+{{--
+  Template Name: Events
+--}}
+
+@extends('layouts.app')
+
+@section('content')
+  <div id="tribe-events-pg-template">
+	<?php tribe_events_before_html(); ?>
+	<?php tribe_get_view(); ?>
+	<?php tribe_events_after_html(); ?>
+  </div> <!-- #tribe-events-pg-template -->
+@endsection
