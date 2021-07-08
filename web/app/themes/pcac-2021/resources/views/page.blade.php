53e585301e60206f8072ed31bb8c7e04a18a21b9
Francois Huyghe <francois@13milliseconds.com>
2021-07-08 16:04:31 -0400

    council pages edits

--- web/app/themes/pcac-2021/resources/views/page.blade.php
+++ web/app/themes/pcac-2021/resources/views/page.blade.php
@@ -2,9 +2,7 @@
 
 @section('content')
   @while(have_posts()) @php the_post() @endphp
-    <div class="container">
       @include('partials.page-header')
       @include('partials.content-page')
-    </div>
   @endwhile
 @endsection
--- web/app/themes/pcac-2021/resources/views/partials/page-header.blade.php
+++ web/app/themes/pcac-2021/resources/views/partials/page-header.blade.php
@@ -1,11 +1,16 @@
 <div class="page-header">
   @if($post->post_parent)
   <div class="parent-banner {{ get_post_field( 'post_name', $post->post_parent) }}">
-    <a href="{{ get_the_permalink($post->post_parent) }}" class="council-logo">
-    {!! file_get_contents(App\asset_path('images/logo_PCAC.svg')) !!}
-    {{ get_the_title($post->post_parent) }}
-    </a>
+    <div class="container">
+      <a href="{{ get_the_permalink($post->post_parent) }}" class="council-logo">
+      {!! file_get_contents(App\asset_path('images/logo_PCAC.svg')) !!}
+      {{ get_the_title($post->post_parent) }}
+      </a>
+    </div>
   </div>
   @endif
-  <h1>{!! App::title() !!}</h1>
+
+  <div class="container">
+    <h1>{!! App::title() !!}</h1>
+  </div>
 </div>
